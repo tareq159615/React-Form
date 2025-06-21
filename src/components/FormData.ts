9bb816e343db36186b2d04879dc9c6d82f2bf676
Tareq Aziz <125204331+tareq159615@users.noreply.github.com>
2025-06-21 23:16:37 +0600

    first commit

---
+++ src/components/FormData.ts
@@ -0,0 +1,12 @@
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  city: string;
+  state: string;
+  zip: string;
+  country: string;
+  completeLocation: string;
+}
+
+export type{FormData}
